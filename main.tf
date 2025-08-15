d9f83ac34e5ed7b4a141117b8717ab0aa0ddd5e9
jebbane mohammed <jebbanelab@gmail.com>
2025-08-16 01:19:11 +0200

    add files main.tf et main.yaml

---
+++ main.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_security_group" "nginx" {
+  name   = "nginx_access"
+
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
