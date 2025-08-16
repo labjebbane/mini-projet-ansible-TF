bc1dda351848197948cf5026c6d6daf240f7226f
jebbane mohammed <jebbanelab@gmail.com>
2025-08-16 21:26:53 +0200

    part 2 update main task

--- main.tf
+++ main.tf
@@ -2,9 +2,58 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# Création du VPC
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+# Subnet public
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+# Route table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public-rt"
+  }
+}
+
+# Association route table avec subnet
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+# Security Group (SSH + HTTP)
 resource "aws_security_group" "nginx" {
   name   = "nginx_access"
-
+  vpc_id = aws_vpc.main.id
 
   ingress {
     from_port   = 22
@@ -26,4 +75,39 @@ resource "aws_security_group" "nginx" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    Name = "nginx-sg"
+  }
 }
+
+# Instance EC2
+resource "aws_instance" "nginx" {
+  ami                         = "ami-020cba7c55df1f615"
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public.id
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.nginx.id]
+  key_name                    = "devops" 
+
+  provisioner "remote-exec" {
+    inline = ["echo 'Wait until SSH is ready'"]
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("~/Downloads/devops.pem")
+      host        = self.public_ip
+    }
+  }
+
+  provisioner "local-exec" {
+    command = "ansible-playbook -i ${self.public_ip}, --private-key ~/Downloads/devops.pem nginx.yaml"
+  }
+
+  tags = {
+    Name = "nginx-instance"
+  }
+}
+
+
